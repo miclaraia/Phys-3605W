5f329755b0af050885a0c4368f190572e1da7323
Michael Laraia <micheal.laraia@gmail.com>
2018-02-05 23:33:30 -0600

    Added data and started lab report graphs

--- 1_photoelectric/second.m
+++ 1_photoelectric/second.m
@@ -1,16 +1,10 @@
-more off
-A=[579.05 369; 579.05 369; 579.05 369.5;
-    576.96 377.5; 576.96 376.5; 576.96 376;
-    546.06 486.5; 546.06 487.5; 546.06 488; 546.06 488;
-    491.6 681; 491.6 686.5; 491.6 685;
-    435.84 884; 435.84 886; 435.84 885;
-    434.75 888.5;
-    433.92 891.5;
-    407.78 992; 407.78 985;
-    404.66 999.5; 404.66 995; 404.66 995.5;
-    ]
-A=csvread('calibration.csv',1)
+l=3;
+w=2;
+
 dial_error=6
+voltage_error=.01
+
+A=csvread('calibration.csv',1)
 
 x=A(:,1)
 yerr=dial_error*ones(23,1)
@@ -23,7 +17,7 @@ B=[ a b ]';
 
 xerr=sqrt(yerr.^2/b^2+aerr^2/b^2+berr^2*y.^2/b^4)
 
-subplot(2,2,1);
+subplot(l,w,1);
 p=plot(x, y, 'b.');
 p.MarkerSize=10;
 hold on;
@@ -42,14 +36,15 @@ hold off;
 Chi=(y-(a+b.*x))./yerr
 Chi2=Chi.^2
 
-ax=subplot(2,2,2)
+ax=subplot(l,w,2)
 hist(ax, Chi)
 %hold on
 % plot(x, Chi2)
 Chi2=sum(Chi2)
 %%
+
+ % Curve 1, .5 mm
 data=csvread('curve_1.csv',1)
-voltage_error=.01
 x=data(:,1);
 y=data(:,2);
 d=size(x)
@@ -57,9 +52,9 @@ xerr=dial_error*ones(d);
 yerr=voltage_error*ones(d);
 
 [a,aerr,b,berr]=my_fit(x, y, yerr)
-B=[ a b ]';
+B1=[ a b ]';
 
-subplot(2,2,3);
+subplot(l,w,3);
 plot(x, y, '.');
 hold on
 %l=errorbar(x,y, yerr);
@@ -69,42 +64,61 @@ hold on
 %l.LineStyle='none';
 %l.Color='b';
 
-data=csvread('curve_2.csv', 1)
+
+data=csvread('curve_2.csv',1)
 x=data(:,1);
 y=data(:,2);
 d=size(x)
 xerr=dial_error*ones(d);
 yerr=voltage_error*ones(d);
 
-fit=linspace(min(x),max(x),10)';
-h=plot(fit, [ones(size(fit)) fit]*B, '-r');
-
-% Curve 2, .5mm
 [a,aerr,b,berr]=my_fit(x, y, yerr)
-B=[ a b ]';
-
-h=plot(x, y, 'm.');
-%l=errorbar(x,y, yerr);
-%l.LineStyle='none';
-%l.Color='b';
-%l=errorbar(x,y, xerr, 'horizontal');
-%l.LineStyle='none';
-%l.Color='b';
+B2=[ a b ]';
+plot(x, y, 'm.');
 
 fit=linspace(min(x),max(x),10)';
-plot(fit, [ones(size(fit)) fit]*B, '-c');
-uistack(h, 'top')
+plot(fit, [ones(size(fit)) fit]*B1, '-r');
+plot(fit, [ones(size(fit)) fit]*B2, '-c');
 hold off
- %%%%%%%%%%%%%%%%%%%%%%%
 
-subplot(2,2,4)
+subplot(l,w,5)
 Chi=(y-(a+b.*x))./yerr;
 [x xerr y yerr Chi]
 hist(Chi)
 %hold on
 % plot(x, Chi2)
 Chi2=sum(Chi.^2)
-print('Plots', '-dpng')
+ %%%%%%%%%%%%%%%%%%%%%%%
+
+
+data=csvread('curve_3.csv',1)
+x=data(:,1);
+y=data(:,2);
+d=size(x)
+xerr=dial_error*ones(d);
+yerr=voltage_error*ones(d);
+
+[a,aerr,b,berr]=my_fit(x, y, yerr)
+B3=[ a b ]';
+
+subplot(l,w,4)
+plot(x, y, 'm.');
+hold on
+
+fit=linspace(min(x),max(x),10)';
+plot(fit, [ones(size(fit)) fit]*B3, '-c');
+hold off
+
+subplot(l,w,6)
+Chi=(y-(a+b.*x))./yerr;
+[x xerr y yerr Chi]
+hist(Chi)
+%hold on
+% plot(x, Chi2)
+Chi2=sum(Chi.^2)
+
+
+print('second', '-dpng')
 %%
 
 
